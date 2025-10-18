a47954f42370a3abbf27d4ffe69fb4f89753f054
Leandro Demian Gaona Obispo <20243rd051@utez.edu.mx>
2025-10-17 19:27:10 -0600

    Se añadió ensure-session-state() y new-secret()

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, request, render_template_string, session, jsonify, redirect, url_for
 import os
-from flask import Flask
-
+import random
+from flask import Flask, session
 app = Flask(__name__)
 app.secret_key = os.environ.get(&quot;FLASK_SECRET_KEY&quot;, &quot;cambia-esta-
 clave&quot;)
+# -----------------------------
+# Utilidades de estado (por sesión)
+# -----------------------------
+# Función 1: Asegura que las variables de sesión estén inicializadas
+(puntos, intentos, número secreto, estado del juego)
+# Autor: Marvin Rafael
+def ensure_session_state():
+session.setdefault(&quot;points&quot;, 0)
+session.setdefault(&quot;tries&quot;, 0)
+session.setdefault(&quot;secret&quot;, random.randint(1, 100))
+session.setdefault(&quot;game_over&quot;, False)
+# Función 2: Genera un nuevo número secreto aleatorio entre 1 y 100
+# Autor: Leandro Demian
+def new_secret():
+session[&quot;secret&quot;] = random.randint(1, 100)
+
 @app.route(&quot;/&quot;)
 def root():
 return &quot;OK&quot;
