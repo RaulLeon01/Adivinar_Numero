a00083ff76f29f751a6cc485a557c231c61cc93f
Leandro Demian Gaona Obispo <20243rd051@utez.edu.mx>
2025-10-17 19:24:03 -0600

    app Flask minima con ruta raiz

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ from flask import Flask, request, render_template_string, session, jsonify, redi
 import os
 from flask import Flask
 
+app = Flask(__name__)
+app.secret_key = os.environ.get(&quot;FLASK_SECRET_KEY&quot;, &quot;cambia-esta-
+clave&quot;)
+@app.route(&quot;/&quot;)
+def root():
+return &quot;OK&quot;
+if __name__ == &quot;__main__&quot;:
+app.run(debug=True, host=&quot;0.0.0.0&quot;, port=5000)
